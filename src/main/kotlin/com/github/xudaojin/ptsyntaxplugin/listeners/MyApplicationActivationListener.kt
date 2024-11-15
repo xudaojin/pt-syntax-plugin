0095fb78885898f503d083429c099b351e9b52e3
GitHub Action <action@github.com>
2024-11-15 07:13:00 +0000

    Template cleanup

--- src/main/kotlin/com/github/xudaojin/ptsyntaxplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/xudaojin/ptsyntaxplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.xudaojin.ptsyntaxplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
